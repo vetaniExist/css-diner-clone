ef0c7611e9713b92281d99fe7cc8ea9954ecf153
Huriosity <vetani3123@mail.ru>
2020-11-30 21:19:56 +0300

    init: start rs-css

---
+++ rs-css/.eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+        "extends": [
+            "airbnb-base"
+            //"prettier"
+        ],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "rules": {
+        "linebreak-style": ["error", "windows"],
+        "quotes": ["error", "double"],
+        "max-len": ["error", { "code": 120 }],
+        "no-loop-func": 1,
+    }
+};
---
+++ rs-css/webpack.config.js
@@ -0,0 +1,74 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+
+module.exports = (env, options) => {
+    const isProd = options.mode === 'production';
+
+    const config = {
+        mode: isProd ? 'production' : 'development',
+        devtool: isProd ? 'cheap-module-source-map' : 'source-map',
+        watch: !isProd, 
+        entry: ['./src/js/index.js', './src/css/style.css', './src/css/normalize.css'],
+        output: {
+            filename: 'main3.js',
+            path: path.resolve(__dirname, 'dist'),
+        },
+
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env'],
+                            plugins: [
+                                ["@babel/transform-runtime"]
+                            ]
+                        }
+                    }
+                },
+                {
+                    test: /\.css$/,
+                    use: [
+                        /*'style-loader'*/ MiniCssExtractPlugin.loader,'css-loader', /*'sass-loader'*/
+                    ]
+                },
+                {
+                    test: /\.(png|jpe?g|gif|svg|jpg|wav)$/i,
+                    use: [
+                        {
+                            loader: 'file-loader',
+                            options: {
+                                publicPath: 'assets/',
+                                outputPath: './assets/',
+                            },
+                        },
+                    ],
+                },
+                {
+                    test: /\.html$/i,
+                    loader: 'html-loader',
+                },
+            ]
+        },
+
+        plugins:[
+            new CleanWebpackPlugin(),
+            new HtmlWebpackPlugin({
+                template: 'src/index.html',
+                // favicon: 'src/assets/favicon.svg',
+            }),
+            new MiniCssExtractPlugin({
+                filename: 'style.css',
+            }),
+           
+            new ESLintPlugin()
+        ]
+    }
+    return config;    
+};
