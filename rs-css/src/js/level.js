a6354cfb8389fa5557e8e9c72fab0792b417147a
Huriosity <vetani3123@mail.ru>
2020-12-02 12:17:43 +0300

    refactor: code cleaning

--- rs-css/src/js/level.js
+++ rs-css/src/js/level.js
@@ -1,5 +1,38 @@
 import { createEl } from "./layout";
 
+function createNode(str, tabs) {
+  // split on classes;
+  str = str.split(".");
+  const classes = str.slice(1, str.length);
+
+  // find id and tag
+  str = str[0].split("#");
+  const id = str[1];
+  const tag = str[0];
+
+  const newEl = createEl(tag);
+  // newEl.innerText = `<${tag}`;
+  newEl.innerText = "\t".repeat(tabs).concat(`<${tag}\n`);
+  if (id) {
+    newEl.innerText += " id = \"".concat(id).concat("\"");
+    newEl.setAttribute("id", id);
+  }
+
+  if (classes.length) {
+    newEl.innerText += " class = ";
+    for (let i = 0; i < classes.length; i += 1) {
+      newEl.innerText += `"${classes[i]}"`;
+      newEl.classList.add(classes[i]);
+      if (i !== classes.length - 1) {
+        newEl.innerText += " ";
+      }
+    }
+  }
+  newEl.innerText += ">";
+  newEl.setAttribute("class", "block");
+  return newEl;
+}
+
 export class Level {
   constructor() {
     this.levelHtml = null;
@@ -14,12 +47,11 @@ export class Level {
   }
 
   parseTemplateString(str) {
-    console.log("parseTemplateString");
-    console.log(str);
-
+    // console.log("parseTemplateString");
+    // console.log(str);
     const splitedByBasicDelemeter = str.split(";");
     let tabsCounter = 0;
-    const result = this.createNode("div");
+    const result = createNode("div");
     let resultPrev = {
       curNode: null,
       prevNode: null,
@@ -37,26 +69,15 @@ export class Level {
         resultPrev = resultPrev.prevNode;
         continue;
       }
-      //console.log(splitedByBasicDelemeter[z]);
       const splitedByDeepLevels = splitedByBasicDelemeter[z].split(">");
 
-      let currentDeep;
       for (let i = 0; i < splitedByDeepLevels.length; i += 1) {
-        // console.log("  ".repeat(i) +  splitedByDeepLevels[i]);
         const splitedInSingleLevel = splitedByDeepLevels[i].split(",");
 
-
         for (let j = 0; j < splitedInSingleLevel.length; j += 1) {
-          curNode = this.createNode(splitedInSingleLevel[j], tabsCounter); 
-          // currentDeep.appendChild(curNode);
-          // console.log("  ".repeat(tabsCounter) + splitedInSingleLevel[j] + j + i);
-          // console.log(curNode);
-
+          curNode = createNode(splitedInSingleLevel[j], tabsCounter);
           // зашли в цикл добавили ноды первого уровня в result, поставили указатель resultPrev на эту ноду
-          // console.log(i, j);
-          // console.log(splitedByDeepLevels.length, splitedInSingleLevel.length);
-          if (tabsCounter === 0 ) {
-            //console.log("  ".repeat(tabsCounter) + splitedInSingleLevel[j] + j + i);
+          if (tabsCounter === 0) {
             result.appendChild(curNode);
             resultPrev.curNode = curNode;
             resultPrev.prevNode = curNode;
@@ -64,20 +85,15 @@ export class Level {
             resultPrev.curNode.appendChild(curNode);
           }
         }
-       
+
         if (i !== splitedByDeepLevels.length - 1) {
           tabsCounter += 1;
-          // prevNode = resultPrev;
           const newResult = {
-            curNode: curNode,
+            curNode,
             prevNode: resultPrev,
-          }
+          };
           resultPrev = newResult;
-          //resultPrev.prevNode = curNode;
-          // resultPrev.curNode = curNode;
-
         }
-
       }
     }
     console.log("result");
@@ -85,40 +101,6 @@ export class Level {
     console.log("\n\n\n\n\n\n");
     console.log("\n\n\n\n\n\n");
     return result;
-  }
-
-  createNode(str, tabs) {
-    // split on classes;
-    str = str.split(".");
-    const classes = str.slice(1, str.length);
-
-    // find id and tag 
-    str = str[0].split("#");
-    const id = str[1];
-    const tag = str[0];
-
-    const newEl = createEl(tag);
-    // newEl.innerText = `<${tag}`;
-    newEl.innerText = "\t".repeat(tabs) + `<${tag}\n`;
-    if (id) {
-      newEl.innerText += " id = \"" + id + "\"";
-      newEl.setAttribute("id", id);
-    }
-
-    if (classes.length) {
-      newEl.innerText += " class = ";
-      for (let i = 0; i < classes.length; i += 1) {
-        newEl.innerText += `"${classes[i]}"`;
-        newEl.classList.add(classes[i]);
-        if (i !== classes.length - 1) {
-          newEl.innerText += " ";
-        }
-      }
-    }
-    newEl.innerText += ">";
-    newEl.setAttribute("class", "block");
-    return newEl;
-
   }
 
   configurateLevelFromString(level, str) {
